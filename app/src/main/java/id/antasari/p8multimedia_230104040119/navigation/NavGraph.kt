87881d645bfe10f21e4374dbacfa5e626529651f
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 22:45:52 +0800

    Create NavGraph.kt

---
+++ app/src/main/java/id/antasari/p8multimedia_230104040119/navigation/NavGraph.kt
@@ -0,0 +1,8 @@
+package id.antasari.p8multimedia_230104040119.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NavGraph() {
+    // Mengatur navigasi antar screen multimedia
+}
