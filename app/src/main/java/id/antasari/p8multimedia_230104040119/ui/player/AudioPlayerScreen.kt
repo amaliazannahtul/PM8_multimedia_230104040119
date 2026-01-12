26e6cd432a6fb6b9ad4fb9f53f4aae687ce681a1
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 22:41:52 +0800

    Create AudioPlayerScreen.kt

---
+++ app/src/main/java/id/antasari/p8multimedia_230104040119/ui/player/AudioPlayerScreen.kt
@@ -0,0 +1,12 @@
+package id.antasari.p8multimedia_230104040119.ui.player
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AudioPlayerScreen() {
+    // Screen untuk memutar audio
+    // Konsep:
+    // - MediaPlayer
+    // - Play / Pause audio
+    // - Memutar file audio dari storage
+}
