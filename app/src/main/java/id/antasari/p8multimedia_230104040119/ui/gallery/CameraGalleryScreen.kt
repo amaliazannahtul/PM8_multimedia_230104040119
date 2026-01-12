40d704d1633a7057a73f3962b1e9612353162820
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 22:44:20 +0800

    Create CameraGalleryScreen.kt

---
+++ app/src/main/java/id/antasari/p8multimedia_230104040119/ui/gallery/CameraGalleryScreen.kt
@@ -0,0 +1,12 @@
+package id.antasari.p8multimedia_230104040119.ui.gallery
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraGalleryScreen() {
+    // Screen untuk kamera dan galeri
+    // Konsep:
+    // - Intent Camera
+    // - MediaStore
+    // - Menampilkan foto dari galeri
+}
