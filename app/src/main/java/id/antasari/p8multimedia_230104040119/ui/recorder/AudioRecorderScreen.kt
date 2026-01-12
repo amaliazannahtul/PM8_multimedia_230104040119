fb1269f26b771e0a0e9ab2e7edabdcf4a9651804
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 22:38:51 +0800

    Create AudioRecorderScreen.kt

---
+++ app/src/main/java/id/antasari/p8multimedia_230104040119/ui/recorder/AudioRecorderScreen.kt
@@ -0,0 +1,12 @@
+package id.antasari.p8multimedia_230104040119.ui.recorder
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AudioRecorderScreen() {
+    // Screen untuk merekam audio
+    // Konsep:
+    // - Menggunakan MediaRecorder
+    // - Request permission RECORD_AUDIO
+    // - Simpan file audio ke storage
+}
