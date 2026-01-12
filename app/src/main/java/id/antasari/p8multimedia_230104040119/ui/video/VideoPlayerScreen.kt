f06a0a5d990a6fee05e4ba7fa6a1a33d97b72409
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 22:43:08 +0800

    Create VideoPlayerScreen.kt

---
+++ app/src/main/java/id/antasari/p8multimedia_230104040119/ui/video/VideoPlayerScreen.kt
@@ -0,0 +1,12 @@
+package id.antasari.p8multimedia_230104040119.ui.video
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun VideoPlayerScreen() {
+    // Screen untuk memutar video
+    // Konsep:
+    // - ExoPlayer
+    // - VideoView
+    // - Kontrol play, pause, fullscreen
+}
