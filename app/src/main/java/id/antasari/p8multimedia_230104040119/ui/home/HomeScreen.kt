ee0dd5f0ecd911b3dac221dd35b465e4995b78b1
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 22:36:42 +0800

    Create HomeScreen.kt

---
+++ app/src/main/java/id/antasari/p8multimedia_230104040119/ui/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package id.antasari.p8multimedia_230104040119.ui.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    // Home screen multimedia
+    // Menu:
+    // - Audio Recorder
+    // - Audio Player
+    // - Video Player
+    // - Camera & Gallery
+}
